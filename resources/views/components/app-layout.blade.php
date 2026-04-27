516a9e7bc7b2fb6890c343df65458a674d293cc1
N0m@d-Xii <hugewolf1996@gmail.com>
2026-04-27 21:23:23 +0700

    ติดตั้ง DaisyUI

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -19,18 +19,22 @@
     <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
     <div class="min-h-full">
         <nav class="bg-gray-800">
+            {{-- Desktop --}}
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div class="flex h-16 items-center justify-between">
+
                     <div class="flex items-center">
+                        {{-- Logo  --}}
                         <div class="shrink-0">
                             <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
                                 alt="Your Company" class="size-8" />
                         </div>
+
                         <div class="hidden md:block">
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
-                                <a href="#" aria-current="page"
-                                    class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Dashboard</a>
+                                <a href="/" aria-current="page"
+                                    class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">หน้าแรก</a>
                                 <a href="#"
                                     class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Team</a>
                                 <a href="#"
@@ -42,6 +46,7 @@
                             </div>
                         </div>
                     </div>
+
                     <div class="hidden md:block">
                         <div class="ml-4 flex items-center md:ml-6">
                             <button type="button"
@@ -62,9 +67,16 @@
                                     class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
                                     <span class="absolute -inset-1.5"></span>
                                     <span class="sr-only">Open user menu</span>
-                                    <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                    <div class="flex items-center gap-4">
+  <img class="w-10 h-10 rounded-full" src="avatar.jpg" alt="User">
+  <div class="font-medium dark:text-white">
+    <div>Jane Doe</div>
+    <div class="text-sm text-gray-500 dark:text-gray-400">Software Engineer</div>
+  </div>
+</div>
+                                    {{-- <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                                         alt=""
-                                        class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
+                                        class="size-8 rounded-full outline -outline-offset-1 outline-white/10" /> --}}
                                 </button>
 
                                 <el-menu anchor="bottom end" popover
@@ -81,6 +93,7 @@
                             </el-dropdown>
                         </div>
                     </div>
+
                     <div class="-mr-2 flex md:hidden">
                         <!-- Mobile menu button -->
                         <button type="button" command="--toggle" commandfor="mobile-menu"
@@ -100,7 +113,7 @@
                     </div>
                 </div>
             </div>
-            {{-- Mobile Menu --}}
+            {{-- Mobile  --}}
             <el-disclosure id="mobile-menu" hidden class="block md:hidden">
                 <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
@@ -117,11 +130,13 @@
                 </div>
                 <div class="border-t border-white/10 pt-4 pb-3">
                     <div class="flex items-center px-5">
-                        <div class="shrink-0">
+                        {{-- รูปโปลไฟล์ Mobile --}}
+                        {{-- <div class="shrink-0">
                             <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                                 alt=""
                                 class="size-10 rounded-full outline -outline-offset-1 outline-white/10" />
-                        </div>
+                        </div> --}}
+
                         <div class="ml-3">
                             <div class="text-base/5 font-medium text-white">Tom Cook</div>
                             <div class="text-sm font-medium text-gray-400">tom@example.com</div>
