eecab04ca94fa4000aad962e4d3942d73e236f7f
Nuttapong <hugewolf1996@gmail.com>
2026-05-06 14:18:58 +0700

    สร้าง model , migration , controller ของ room แล้ว

---
+++ app/Models/room.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class room extends Model
+{
+    protected $fillable = [
+        'name',
+        'pic',
+        'color',
+    ];
+}
