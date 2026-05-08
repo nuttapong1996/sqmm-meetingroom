460b0ff65dfa016f0da0d336b8187ddae6a41573
N0m@d-Xii <hugewolf1996@gmail.com>
2026-05-08 20:19:08 +0700

    ทดสอบ middleware ของ api

--- routes/api.php
+++ routes/api.php
@@ -11,15 +11,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/meetings', function (Request $request) {
     // FullCalendar ต้องการรูปแบบ: title, start, end
-    return Meeting::all()->map(function ($meeting) {
-        return [
-            'id'    => $meeting->id,
-            'title' => $meeting->room->name . ' - ' . $meeting->purpose,
-            'start' => $meeting->start_time,
-            'end'   => $meeting->end_time,
-            'color' => $meeting->room->color, // ใช้สีที่เราตั้งค่าไว้ใน DB
-        ];
-    });
+    return 'hello';
 });
+// })->middleware('auth:sanctum');
 
 
