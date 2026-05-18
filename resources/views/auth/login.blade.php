983b2abbce44ca5d1de40943e2a43db3102e8a1a
Nuttapong <hugewolf1996@gmail.com>
2026-05-18 11:33:25 +0700

    แก้ไข layout หน้า login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,23 @@
 <x-app-layout title="เข้าสู่ระบบ">
-    <fieldset class="fieldset bg-white bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <legend class="fieldset-legend text-gray-100 bg-neutral p-2 rounded-3xl">เข้าสู่ระบบ</legend>
-        <form class="fieldset w-xs" action="{{ route('check-emp') }}" method="POST">
-            @csrf
-           <x-error-alert></x-error-alert>
-            <div class="flex flex-col">
-                <label class="label">รหัสพนักงาน</label>
-                <input type="text" class="input" placeholder="รหัสพนักงาน" name="empcode" id="empcode"
-                    autocomplete="off" />
-            </div>
-            {{-- <p class="validator-hint hidden">กรุณากรอกรหัสพนักงาน</p> --}}
+    <div class="flex flex-col justify-center items-center h-100">
+        <fieldset class="fieldset bg-white bg-base-200 border-base-300 rounded-box w-xs border p-4">
+            <legend class="fieldset-legend text-gray-100 bg-neutral p-2 rounded-3xl">เข้าสู่ระบบ</legend>
+            <form class="fieldset w-xs" action="{{ route('check-emp') }}" method="POST">
+                @csrf
+                <x-error-alert></x-error-alert>
+                <div class="flex flex-col">
+                    <label class="label">รหัสพนักงาน</label>
+                    <input type="text" class="input" placeholder="รหัสพนักงาน" name="empcode" id="empcode"
+                        autocomplete="off" />
+                </div>
+                {{-- <p class="validator-hint hidden">กรุณากรอกรหัสพนักงาน</p> --}}
 
-            {{-- <label class="label">Password</label>
+                {{-- <label class="label">Password</label>
         <input type="password" class="input" placeholder="Password" /> --}}
 
-            <button class="btn btn-neutral mt-4" type="submit">Login</button>
-        </form>
+                <button class="btn btn-neutral mt-4" type="submit">Login</button>
+            </form>
 
-    </fieldset>
+        </fieldset>
+    </div>
 </x-app-layout>
